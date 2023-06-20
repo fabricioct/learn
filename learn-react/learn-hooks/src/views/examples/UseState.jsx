304e1ed46a1b9f68530addd03312562af1144d8b
Fabricio C.T <fabriciocotta@gmail.com>
2023-06-20 16:53:12 -0300

    Estudo React

---
+++ learn-react/learn-hooks/src/views/examples/UseState.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import PageTitle from '../../components/layout/PageTitle'
+
+const UseState = (props) => {
+
+    const x = useState();
+
+    return (
+        <div className="UseState">
+            <PageTitle
+                title="Hook UseState"
+                subtitle="Estado em componentes funcionais!"
+            />
+        </div>
+    )
+}
+
+export default UseState
